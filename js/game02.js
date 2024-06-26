66544766c69c7c691f3e58af6e4abfa2d88f2fd1
Denis <denisbormusov@gmail.com>
2024-06-26 20:42:56 +0500

    fixed errors

--- js/game02.js
+++ js/game02.js
@@ -1,89 +1,81 @@
 'use strict'
 
-let userNumberOne;
-let userNumberTwo;
-let continueLoop = true;
-let continueLoopQuestionnaire = false;
+let min, max;
+const interval = [];
 
-while (continueLoop) {
-  if (userNumberOne === undefined || Number.isNaN(+userNumberOne) || userNumberOne.trim() === '') {
-  userNumberOne = prompt('Введите первое число', '');
+while (interval.length < 2 && interval[interval.length - 1] !== null) {
+  const answer = prompt(`Введите ${!interval.length ? "первое" : "второе"} число`, '');
 
-  if (userNumberOne === null) {
-      alert("Отменено. Игра остановлена.");
-      continueLoop = false;
-      continue;  
-  }
-  if (Number.isNaN(+userNumberOne) || userNumberOne.trim() === '') {
-    alert('Введите число!');
-    continue;
-  }
-}
-  userNumberTwo = prompt('Введите второе число', '');
-    
-  if (userNumberTwo === null) {
-      alert("Отменено. Игра остановлена.");
-      continueLoop = false;
-      continue;
-  }
-  if (Number.isNaN(+userNumberTwo) || userNumberTwo.trim() === '') {
-    alert('Введите число!');
-    continue;
+  if (answer === null) {
+    alert('Игра прервана')
   }
 
-  continueLoop = false;
-  continueLoopQuestionnaire = true;
+  if (Number.isNaN(+answer) || answer.trim() === '') {
+    alert('Введите число!')
+  }
+
+  if (!Number.isNaN(+answer) && answer.trim() !== '' && !interval.includes(answer)) {
+    interval.push(answer);
+  }
+
 }
 
-const userAttempts = Math.round((Math.max(userNumberOne, userNumberTwo) - Math.min(userNumberOne, userNumberTwo)) * 0.3);
-const hiddenNumber = Math.floor(Math.random() * (Math.max(userNumberOne, userNumberTwo) - Math.min(userNumberOne, userNumberTwo) + 1))
-+ Math.min(userNumberOne, userNumberTwo);
-const arrayPredictNumber = []; 
-let userPredictNumber;
+[min, max] = interval.sort((a, b) => +a > +b ? 1 : -1);
 
-while (continueLoopQuestionnaire && !(userPredictNumber === null) && !(arrayPredictNumber.length === userAttempts) && !(+userPredictNumber === hiddenNumber)) {
-  userPredictNumber = prompt(`Введите число от ${Math.min(userNumberOne, userNumberTwo)} до ${Math.max(userNumberOne, userNumberTwo)}, у вас ${userAttempts} попытки`, '');
+if (min !== null && max !== null) {
+  const userAttempts = Math.round((max - min) * 0.3);
+  const hiddenNumber = Math.floor(Math.random() * (+max - +min + 1)) + +min;
+  console.log('hiddenNumber: ', hiddenNumber);
+  let continueLoopQuestionnaire = true;
+  const arrayPredictNumber = []; 
+  let userPredictNumber;
 
-  switch (true) {
-    case userPredictNumber === null:
-      alert('Игра закончилась');
-      continueLoopQuestionnaire = false;
-      break;
-
-    case Number.isNaN(+userPredictNumber):
-    case userPredictNumber.trim() === '':
-      alert('Введите число!');
-      break;
-
-    case arrayPredictNumber.includes(+userPredictNumber):
-      alert('Это число вы уже вводили');
-      break;
-
-    case userPredictNumber > Math.max(userNumberOne, userNumberTwo):
-    case userPredictNumber < Math.min(userNumberOne, userNumberTwo):
-      alert(`Число ${userPredictNumber} не входит в диапазон чисел от ${Math.min(userNumberOne, userNumberTwo)}
-        до ${Math.max(userNumberOne, userNumberTwo)}`);
-      break;
-
-    case userPredictNumber > hiddenNumber && !(arrayPredictNumber.includes(+userPredictNumber)):
-      arrayPredictNumber.push(+userPredictNumber);
-      alert(`Загаданное число меньше ${userPredictNumber}!`);
-      if (arrayPredictNumber.length === userAttempts) {
-        alert('Попытки закончились');
-      }
-      break;
-
-    case userPredictNumber < hiddenNumber && !(arrayPredictNumber.includes(+userPredictNumber)):
-      arrayPredictNumber.push(+userPredictNumber);
-      alert(`Загаданное число больше ${userPredictNumber}!`);
-      if (arrayPredictNumber.length === userAttempts) {
-        alert('Попытки закончились');
-      }
-      break;
-      
-    default:
-      alert(`Вы угадали это число ${hiddenNumber}`);
-      break;   
+  while (continueLoopQuestionnaire) {
+    userPredictNumber = prompt(`Введите число от ${min} до ${max}, у вас ${userAttempts} попытки`, '');
+  
+    switch (true) {
+      case userPredictNumber === null:
+        alert('Игра закончилась');
+        continueLoopQuestionnaire = false;
+        break;
+  
+      case Number.isNaN(+userPredictNumber):
+      case userPredictNumber.trim() === '':
+        alert('Введите число!');
+        break;
+  
+      case arrayPredictNumber.includes(+userPredictNumber):
+        alert('Это число вы уже вводили');
+        break;
+  
+      case userPredictNumber > max:
+      case userPredictNumber < min:
+        alert(`Число ${userPredictNumber} не входит в диапазон чисел от ${min} до ${max}`);
+        break;
+  
+      case userPredictNumber > hiddenNumber:
+        arrayPredictNumber.push(+userPredictNumber);
+        alert(`Загаданное число меньше ${userPredictNumber} !`);
+        if (arrayPredictNumber.length === userAttempts) {
+          continueLoopQuestionnaire = false;
+          alert('Попытки закончились');
+        }
+        break;
+  
+      case userPredictNumber < hiddenNumber:
+        arrayPredictNumber.push(+userPredictNumber);
+        alert(`Загаданное число больше ${userPredictNumber} !`);
+        if (arrayPredictNumber.length === userAttempts) {
+          continueLoopQuestionnaire = false;
+          alert('Попытки закончились');
+        }
+        break;
+        
+      default:
+        continueLoopQuestionnaire = false;
+        alert(`Вы угадали это число ${hiddenNumber}`);
+        break;   
+    }
   }
-}
 
+}
